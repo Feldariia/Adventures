d22efb073fca32764395b6a1cbb8a5941f6fc41d
shannonmong <shannonmong@192.168.0.81>
2019-09-07 23:14:08 -0400

    Ready to grade (I think ;_;)

---
+++ Mong Hello There Class 2/src/hellothere.java
@@ -0,0 +1,15 @@
+
+public class hellothere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Project Name: Hello there
+		//Name: Shannon Mong
+		// I tried :(
+		// GitHub is not working and its making me shook
+		System.out.print("\tOh my gosh, I love this class\n\tIs it past add/drop?\n\tI'm just joking, I love this class.\n\tI enjoy this new method of coding\n\tBut it seems somewhat run on\n\tUntil I put it all together in run\n\tBut then again,\n\tI always have enjoyed coding programs\n\tEven if I don't understand much\n\tI still will try my hardest :) ");
+	
+	}
+
+}
