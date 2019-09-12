dccaab86066d349e3b9d57e8aa812718dd5ccd13
shannonmong <shannonmong@192.168.0.81>
2019-09-11 20:25:19 -0400

    Program 1

--- Mong Hello There Class 2/src/hellothere.java
+++ Mong Hello There Class 2/src/hellothere.java
@@ -1,13 +1,17 @@
+         /*
+		 Project Name: Hello there
+		 Author: Shannon Mong
+		 GitHub: Feldariia
+		 Description: Homework 1 for Programming class.
+		 Last Modified: 10Sep2019
+		 Version: 1.0
+		 */
 
 public class hellothere {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Project Name: Hello there
-		//Name: Shannon Mong
-		// I tried :(
-		// GitHub is not working and its making me shook
 		System.out.print("\tOh my gosh, I love this class\n\tIs it past add/drop?\n\tI'm just joking, I love this class.\n\tI enjoy this new method of coding\n\tBut it seems somewhat run on\n\tUntil I put it all together in run\n\tBut then again,\n\tI always have enjoyed coding programs\n\tEven if I don't understand much\n\tI still will try my hardest :) ");
 	
 	}
